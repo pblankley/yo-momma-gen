e285a93e374a7c9badc25543b2c969b21bccdb1a
pblankley <31221512+pblankley@users.noreply.github.com>
2019-04-04 09:03:07 -0400

    add dropout and make a new model

--- yo_momma_api.py
+++ yo_momma_api.py
@@ -1,7 +1,7 @@
 from textgenrnn import textgenrnn
 
 textgen = textgenrnn()
-textgen.train_from_file('jokes.txt', num_epochs=500)
+textgen.train_from_file('jokes.txt', new_model=True, num_epochs=500, train_size=0.9, dropout=0.1)
 #textgen.generate()
 
 #textgen_2 = textgenrnn('/weights/hacker_news.hdf5')
